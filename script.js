3a106dcc6768928b98ed82ca4653cdd8d5f5e1a6
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 19:27:56 -0400

    add feature: pressing number buttons enters numbers into input box

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function mul(x, y) { return x * y }
 function div(x, y) { return x / y }
 
 function operate(operator, x, y) { return operator(x, y) }
+
+// Web functionality
+const screen = document.querySelector('.screen');
+
+const numButtons = document.querySelector('.numbers');
+numButtons.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        screen.value += e.target.textContent;
+    }
+});
