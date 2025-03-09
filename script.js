c4400637ac2e1206e598d4bd0c29b867d23db525
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 17:01:59 -0400

    Initial commit
    Add script.js with add, sub, mul, div, and operate. Add html with link to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Calculator functions
+function add(x, y) { return x + y }
+function sub(x, y) { return x - y }
+function mul(x, y) { return x * y }
+function div(x, y) { return x / y }
+
+function operate(operator, x, y) { return operator(x, y) }
