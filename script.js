5c1506b1a813f6df093646c59b6c79046326831a
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 20:17:42 -0400

    add operator button functionality (!!!)

--- script.js
+++ script.js
@@ -16,7 +16,39 @@ numButtons.addEventListener('click', (e) => {
     }
 });
 
+const opButtons = document.querySelector('.operations');   
+let operator = arg1 = arg2 = null;
+const opMap = {
+    '+' : add,
+    '-' : sub,
+    '*' : mul,
+    '/' : div,
+    '=' : operate,
+}
+opButtons.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        let parsedScreenValue = parseInt(screen.value);
+        if (arg1 === null) {
+            arg1 = parsedScreenValue;
+        } else {
+            arg2 = parsedScreenValue;
+        }
+
+        let opSymb = e.target.value;
+        if (opSymb == '=') {
+            let result = operate(operator, arg1, arg2);
+            screen.value = result;
+            // Prepare for next inputs
+            arg1 = result;
+        } else {
+            operator = opMap[opSymb];
+            screen.value = '';
+        }
+    }
+});
+
 const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', (e) => {
     screen.value = '';
+    operator, arg1, arg2 = null;
 });
